53baa0988c9e8d24da9f4006bcd4c7e3dbb08859
noyon-jethitech <noyon.ahammad@jethitech.com>
2024-03-23 03:12:15 -0400

    fix:middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks(.*)"],s
+  publicRoutes: ["/api/webhooks(.*)"],
 });
 
 export const config = {
