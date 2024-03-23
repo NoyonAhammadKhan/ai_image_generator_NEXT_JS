a803dfb81899296cf7938b9575ce8fd766eb6282
noyon-jethitech <noyon.ahammad@jethitech.com>
2024-03-23 03:05:12 -0400

    implement clerk webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/contact", "/api(.)*"],
+  publicRoutes: ["/api/webhooks(.*)"],s
 });
 
 export const config = {
