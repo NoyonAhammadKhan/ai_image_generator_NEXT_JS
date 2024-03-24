3212a6e6c122b0db2a1fe9d09c80a30cb5c0e135
noyon-jethitech <noyon.ahammad@jethitech.com>
2024-03-24 02:46:03 -0400

    code update

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -1,6 +1,7 @@
 import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
+
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
@@ -9,21 +10,25 @@ interface MongooseConnection {
 let cached: MongooseConnection = (global as any).mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) throw new Error("MONGODB_URL is not defined.");
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "ai_image_genearotor",
+      dbName: "imaginify",
       bufferCommands: false,
     });
 
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
