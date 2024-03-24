c10eed1dd2d19733e3d64f1688e57bdac96f6b09
noyon-jethitech <noyon.ahammad@jethitech.com>
2024-03-24 01:38:51 -0400

    mongodb credentials update

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,11 +1,33 @@
+import Header from '@/components/shared/Header';
 import React from 'react';
+import { transformationTypes } from '@/constants';
+import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationType = () => {
+
+const AddTransformationTypePage =async({params:{type}}:SearchParamProps) => {
+    const {userId} = auth()
+    const transformation = transformationTypes[type];
+    if(!userId) redirect('/sign-in');
+    console.log("User Id",userId)
+    const user = await getUserById(userId);
+
     return (
         <div>
-            AddTransformationType
+            <Header 
+                title={transformation.title} 
+                subtitle={transformation.subTitle}
+            />
+            <TransformationForm
+             action="Add"
+             userId = {user._id}
+             type={transformation.type as TransformationTypeKey}
+             creditBalance={user.creditBalance}
+            />
         </div>
     );
 };
 
-export default AddTransformationType;
+export default AddTransformationTypePage;
